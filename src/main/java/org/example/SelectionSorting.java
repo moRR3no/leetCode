40480002a9ab650c059a06eb3cae2c4263432d11
AzureAD\KonradPstrongowski <konrad.pstrongowski@itsquad.pl>
2024-11-19 16:43:44 +0100

    Selection sorting implementation

---
+++ src/main/java/org/example/SelectionSorting.java
@@ -0,0 +1,25 @@
+package main.java.org.example;
+
+//sorting algorithm
+
+public class SelectionSorting {
+
+    public void sort (int[] arr) {
+        int arrLength = arr.length;
+
+        for (int i = 0; i < arrLength - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < arrLength; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+}
