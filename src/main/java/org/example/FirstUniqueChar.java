d9b891a1449b0a03617b32acdfa494fcb0cf31af
AzureAD\KonradPstrongowski <konradpstrongowski@gmail.com>
2024-11-22 23:16:48 +0100

    First Unique Char task

--- src/main/java/org/example/FirstUniqueChar.java
+++ src/main/java/org/example/FirstUniqueChar.java
@@ -1,2 +1,32 @@
-package main.java.org.example;public class FirstUniqueChar {
+package main.java.org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://leetcode.com/problems/first-unique-character-in-a-string/
+
+public class FirstUniqueChar {
+
+    public int firstUniqChar(String s) {
+        if (s.length()==1) {
+            return 0;
+        }
+        List<Character> repeatedChars = new ArrayList<>();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (repeatedChars.contains(chars[i])) {
+                continue;
+            }
+            for (int j = i + 1; j < chars.length; j++) {
+                if (chars[i] == chars[j]) {
+                    repeatedChars.add(chars[i]);
+                    break;
+                }
+            }
+            if (!repeatedChars.contains(chars[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
